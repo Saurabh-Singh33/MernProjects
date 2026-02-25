62f25272deb914992fb81e0bd08547411e4d8f02
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-02-25 19:10:48 +0530

    Completed

--- Multi-Page Component/src/pages/Home.jsx
+++ Multi-Page Component/src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import { IoIosHome } from "react-icons/io";
 function Home() {
   return (
-    <div>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h2>Welcome to Product Catalog</h2>
+      <IoIosHome />
     </div>
   );
 }
